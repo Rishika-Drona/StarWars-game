2b96fcaf4da24f2c0c3804af5b611a64a871b51c
Rishika Drona <74383234+Rishika-Drona@users.noreply.github.com>
2023-07-30 20:22:46 -0500

    Star Wars Game.py

--- Star Wars Game.py
+++ Star Wars Game.py
@@ -21,13 +21,13 @@ vertex = ((0,15),(-15,0),(-18,5),(-18,-5),(0,0),(18,-5),(18, 5),(15, 0))
 window.register_shape("player", vertex)
 
 asVertex = ((0, 10), (5, 7), (3,3), (10,0), (7, 4), (8, -6), (0, -10), (-5, -5), (-7, -7), (-10, 0), (-5, 4), (-1, 8))
-window.register_shape("chattan", asVertex)
+window.register_shape("stone", asVertex)
 
 ####################
 # Second Part:
 
 
-class Rishika(turtle.Turtle):
+class user(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
 
@@ -35,20 +35,20 @@ class Rishika(turtle.Turtle):
         self.penup()
 
 
-def rishika1(t1, t2):
+def user1(t1, t2):
     x1 = t1.xcor()
     y1 = t1.ycor()
     
     x2 = t2.xcor()
     y2 = t2.ycor()
     
-    taauko = math.atan2(y1 - y2, x1 - x2)
-    taauko = taauko * 180.0 / 3.14159
+    cal = math.atan2(y1 - y2, x1 - x2)
+    cal = cal * 180.0 / 3.14159
     
-    return taauko
+    return cal
 
 
-player = Rishika()
+player = user()
 player.color("white")
 player.shape("player")
 player.score = 0
@@ -58,7 +58,7 @@ player.score = 0
 
 missiles = []
 for _ in range(3):
-    missile = Rishika()
+    missile = user()
     missile.color("red")
     missile.shape("arrow")
     missile.speed = 1
@@ -66,7 +66,7 @@ for _ in range(3):
     missile.hideturtle()
     missiles.append(missile)
 
-pen = Rishika()
+pen = user()
 pen.color("white")
 pen.hideturtle()
 pen.goto(0, 250)
@@ -76,29 +76,29 @@ pen.write("Score: 0", False, align = "center", font = ("Arial", 24, "normal"))
 # Fourth Part
 
 
-chattans = []
+stones = []
 
 for _ in range(5):   
-    chattan = Rishika()
-    chattan.color("brown")
-    chattan.shape("arrow")
+    stone = user()
+    stone.color("brown")
+    stone.shape("arrow")
 
-    chattan.speed  = random.randint(2, 3)/50
-    chattan.goto(0, 0)
-    taauko = random.randint(0, 260)
+    stone.speed  = random.randint(2, 3)/50
+    stone.goto(0, 0)
+    cal = random.randint(0, 260)
     distance = random.randint(300, 400)
-    chattan.setheading(taauko)
-    chattan.fd(distance)
-    chattan.setheading(rishika1(player, chattan))
-    chattans.append(chattan)
+    stone.setheading(cal)
+    stone.fd(distance)
+    stone.setheading(user1(player, stone))
+    stones.append(stone)
 
 ####################
 #Functions for Defence Part
 
-def baanya():
+def left_turn():
     player.lt(20)
     
-def daanya():
+def right_turn():
     player.rt(20)
     
 def fire_missile():
@@ -112,14 +112,14 @@ def fire_missile():
 
 
 window.listen()
-window.onkey(baanya, "Left")
-window.onkey(daanya, "Right")
+window.onkey(left_turn, "Left")
+window.onkey(right_turn, "Right")
 window.onkey(fire_missile, "space")
 
 ####################
 #Functioning the Code Part-1
 
-sakkyo = False
+winner = False
 while True:
 
     window.update()
@@ -134,17 +134,17 @@ while True:
             missile.hideturtle()
             missile.state = "ready"
 
-    for chattan in chattans:    
-        chattan.fd(chattan.speed)
+    for stone in stones:    
+        stone.fd(stone.speed)
         
         for missile in missiles:
-            if chattan.distance(missile) < 20:
-                taauko = random.randint(0, 260)
+            if stone.distance(missile) < 20:
+                cal = random.randint(0, 260)
                 distance = random.randint(600, 800)
-                chattan.setheading(taauko)
-                chattan.fd(distance)
-                chattan.setheading(rishika1(player, chattan))
-                chattan.speed += 0.01
+                stone.setheading(cal)
+                stone.fd(distance)
+                stone.setheading(user1(player, stone))
+                stone.speed += 0.01
                 
                 missile.goto(600, 600)
                 missile.hideturtle()
@@ -157,21 +157,21 @@ while True:
         ####################
         # Functioning the Code Part-2
 
-        if chattan.distance(player) < 20:
-            taauko = random.randint(0, 260)
+        if stone.distance(player) < 20:
+            cal = random.randint(0, 260)
             distance = random.randint(600, 800)
-            chattan.setheading(taauko)
-            chattan.fd(distance)
-            chattan.setheading(rishika1(player, chattan))
-            chattan.speed += 0.005
-            sakkyo = True
+            stone.setheading(cal)
+            stone.fd(distance)
+            stone.setheading(user1(player, stone))
+            stone.speed += 0.005
+            winner = True
             player.score -= 30
             pen.clear()
             pen.write("Score: {}".format(player.score), False, align = "center", font = ("Arial", 24, "normal"))
-    if sakkyo == True:
+    if winner == True:
         player.hideturtle()
         missile.hideturtle()
-        for a in chattans:
+        for a in stones:
             a.hideturtle()
         pen.clear()
         break
@@ -179,7 +179,7 @@ while True:
 window.mainloop()
 
 
-# In[ ]:
+
 
 
 
